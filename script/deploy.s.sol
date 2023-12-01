915babc28cda8eedbe20497c98586bf9e8316f28
FudgyDRS <yetimonsters@gmail.com>
2023-12-01 06:52:43 +0100

    inint support files

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -32,10 +32,3 @@ contract Deploy is LoadKey {
         vm.stopBroadcast();
     }
 }
-//forge script script/deploy.s.sol:MyScript --rpc-url "https://rpc.apothem.network" --broadcast
-forge create --rpc-url "https://rpc.apothem.network" --private-key d2f29e5f86349ed3d860762bc77365d9fff2f0de6868e70feeb0741c2770b36a flat/EntryPoint.sol:EntryPoint
-    --constructor-args "ForgeUSD" "FUSD" 18 1000000000000000000000 \
-    --private-key <your_private_key> \
-    --etherscan-api-key <your_etherscan_api_key> \
-    --verify \
-    src/MyToken.sol:MyToken
